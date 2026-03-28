f54c5a5dde096701a3fd597e7b1d7fdf23715aff
Ahmed Boghdady <boghdady@Ahmeds-iMac.local>
2026-03-28 08:14:24 +0200

    implement api documentation using swagger

--- src/auth/dtos/auth-response.dto.ts
+++ src/auth/dtos/auth-response.dto.ts
@@ -1,6 +1,16 @@
 import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthResponseDto {
+  @ApiProperty({
+    description: 'access token',
+    example: 'kjdlkjdlskjkaljlkasdjlkadjlkjas..',
+  })
   accessToken: string;
+
+  @ApiProperty({
+    description: 'refresh token',
+    example: 'kjdlkjdlskjkaljlkasdjlkadjlkjas..',
+  })
   refreshToken: string;
 }
--- src/auth/dtos/register.dto.ts
+++ src/auth/dtos/register.dto.ts
@@ -1,16 +1,33 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty({
+    description: 'User name',
+    example: 'John Doe',
+  })
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'john.doe@example.com',
+  })
   @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'User phone number',
+    example: '0120998088',
+  })
   @IsNotEmpty()
   phoneNumber: string;
 
+  @ApiProperty({
+    description: 'User password',
+    example: 'password',
+  })
   @IsNotEmpty()
   password: string;
 }
